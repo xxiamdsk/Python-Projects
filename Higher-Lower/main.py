5a00831baea6077a45955a8055b810be69d5250d
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2024-06-26 02:29:44 -0700

    hgher-Lower

---
+++ Higher-Lower/main.py
@@ -0,0 +1,14 @@
+from random import randint
+
+rnum = randint(0, 100)
+noguesses = 0
+while True:
+    guess = int(input(": "))
+    if guess > rnum:
+        print("Lower")
+    elif guess < rnum:
+        print("Higher")
+    else:
+        print("You win " + str(noguesses) + " guesses used")
+        quit()
+    noguesses += 1   
